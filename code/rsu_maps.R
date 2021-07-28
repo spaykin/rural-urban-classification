2b1c9147448a30949e4bd5068ea5f60eb5697fbe
spaykin <spaykin@uchicago.edu>
2021-07-28 13:44:21 -0500

    Update tract and zip maps

--- code/rsu_maps.R
+++ code/rsu_maps.R
@@ -10,18 +10,7 @@ library(tidyverse)
 
 setwd("~/git/rural-urban-classification")
 
-# # Help function to move geometries 
-# place_geometry = function(geometry, bb, scale_x, scale_y,
-#                           scale_size = 1) {
-#   output_geometry = (geometry - st_centroid(geometry)) * scale_size +
-#     st_sfc(st_point(c(
-#       bb$xmin + scale_x * (bb$xmax - bb$xmin),
-#       bb$ymin + scale_y * (bb$ymax - bb$ymin)
-#     )))
-#   return(output_geometry)
-# }
-
-#### Projections and CRS -----
+#### Projections -----
 
 # Set CRS, lower 48 - EPSG 102003 USA_Contiguous_Albers_Equal_Area_Conic
 crs <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
@@ -51,7 +40,7 @@ alaska.sf <- states.sf %>% filter(STATEFP == "02") %>% st_transform(crs_alaska)
 hawaii.sf <- states.sf %>% filter(STATEFP == "15") %>% st_transform(crs_hawaii)
 
 
-#### Tract Map ----
+#### Tract Maps ----
 
 # Load data
 rucaT <- read.csv("data_final/UrbanSubRural_T.csv")
@@ -76,52 +65,33 @@ usT.sf <- rucaT.sf %>% filter(!STATEFP %in% c("02", "15")) %>%
 
 # Tract Map
 
-# Create color scheme option
+# Color scheme
 # rural, suburban, urban
 rural.cols <- c("#c7eae5", "#5ab4ac", "#01665e")
 
-tract_map <-
+# Map & show missing tracts
+tract_map2 <-
   tm_shape(usT.sf2) +
-  tm_fill(col = "rurality", palette = rural.cols, colorNA = "#c7eae5",
-          title = "Classification", showNA = FALSE) +
+  tm_fill(col = "rurality", palette = rural.cols, colorNA = "black",
+          title = "Classification") +
   tm_shape(states48.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
   tm_layout(frame = FALSE, main.title = "Census Tracts by Rural, Suburban, Urban Classification")
+tmap_save(tract_map_missing, "figs/tract_map_missing.png")
 
-alaskaT_map <-
-  tm_shape(alaskaT.sf) +
-  tm_fill(col = "rurality", palette = rural.cols) +
-  tm_shape(alaska.sf) +
-  tm_borders(alpha = 0.7, lwd = 0.5) +
-  tm_layout(frame = FALSE, legend.show = FALSE)
-alaskaT_map
-
-hawaiiT_map <-
-  tm_shape(hawaiiT.sf) +
-  tm_fill(col = "rurality", palette = rural.cols) +
-  tm_shape(hawaii.sf) +
-  tm_borders(alpha = 0.7, lwd = 0.5) +
-  tm_layout(frame = FALSE, legend.show = FALSE)
-hawaiiT_map
-
-# Save final maps
-tmap_save(tract_map, "figs/tract_map.png")
-tmap_save(alaskaT_map, "figs/alaskaT_map.png")
-tmap_save(hawaiiT_map, "figs/hawaiiT_map.png")
-
-# Troubleshoot - identify missing SD tract?
+# Identify missing tracts in SD and other states
 # SD has 222 census tracts
 sd <- tracts.sf %>% filter(STATEFP == "46") #222
 sd_r <- rucaT %>% filter(str_detect(tractFIPS, "^46")) #222
 sd_r2 <- rucaT.sf %>% filter(str_detect(GEOID, "^46")) #219
 sd_r3 <- usT.sf %>% filter(str_detect(GEOID, "^46")) #219
 
-# What is in orig rucaT, but missing from the sf merge
+# What is in orig rucaT, but missing from the sf merge?
 missing <- data.frame(rucaT$tractFIPS[!(rucaT$tractFIPS %in% rucaT.sf$GEOID)])
 missingList <- rucaT$tractFIPS[!(rucaT$tractFIPS %in% rucaT.sf$GEOID)]
 missingSD <- missing %>% filter(str_detect(rucaT.tractFIPS...rucaT.tractFIPS..in..rucaT.sf.GEOID.., "^46"))
-# 3 observations from WY - 46113940500, 46113940800, 46113940900
-# When do the sf merge, want to keep all variables in y
+
+# Redo sf merge, but keep all variables in rucaT 
 rucaT.sf2 <- merge(tracts.sf, rucaT, by.x = "GEOID", by.y = "tractFIPS", all = TRUE)
 usT.sf2 <- rucaT.sf2 %>% filter(!STATEFP %in% c("02", "15")) %>%
   st_transform(crs)
@@ -132,6 +102,8 @@ rucaT.sf_all$rurality <- ifelse(is.na(rucaT.sf2$rurality), "Rural", rucaT.sf2$ru
 # Check 
 missingAll <- rucaT$tractFIPS[!(rucaT$tractFIPS %in% rucaT.sf_all$GEOID)]
 unique(rucaT.sf_all$rurality)
+
+# New tract datasets - use these
 
 # Alaska tracts
 alaskaT.sf <- rucaT.sf_all %>% filter(STATEFP == "02") %>%
@@ -145,44 +117,113 @@ hawaiiT.sf <- rucaT.sf_all %>% filter(STATEFP == "15") %>%
 usT.sf_all <- rucaT.sf_all %>% filter(!STATEFP %in% c("02", "15")) %>%
   st_transform(crs)
 
+# Map
 alaskaT_map <-
   tm_shape(alaskaT.sf) +
   tm_fill(col = "rurality", palette = rural.cols) +
   tm_shape(alaska.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
   tm_layout(frame = FALSE, legend.show = FALSE)
-alaskaT_map
 
 hawaiiT_map <-
   tm_shape(hawaiiT.sf) +
   tm_fill(col = "rurality", palette = rural.cols) +
   tm_shape(hawaii.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
-  tm_layout(frame = FALSE, legend.show = FALSE)
-hawaiiT_map
+  tm_layout(frame = FALSE, 
+            legend.title.size = 1.5,
+            legend.text.size = 1)
 
-tract_map_all <-
+# US Tract Map
+tract_map_notitle <-
   tm_shape(usT.sf_all) +
   tm_fill(col = "rurality", palette = rural.cols, colorNA = "#c7eae5", showNA = FALSE,
           title = "Classification") +
   tm_shape(states48.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
-  tm_layout(frame = FALSE, main.title = "Census Tracts by Rural, Suburban, Urban Classification")
+  tm_layout(frame = FALSE,
+            legend.title.size = 1.5,
+            legend.text.size = 1)
+tmap_save(tract_map_notitle, "figs/tract_map_notitle.png")
 
-tmap_save(tract_map_all, "figs/tract_map.png")
-                       
-
-
-tract_map2 <-
-  tm_shape(usT.sf2) +
-  tm_fill(col = "rurality", palette = rural.cols, colorNA = "black",
+# No legend
+tract_map_all_nolegend <-
+  tm_shape(usT.sf_all) +
+  tm_fill(col = "rurality", palette = rural.cols, colorNA = "#c7eae5", showNA = FALSE,
           title = "Classification") +
   tm_shape(states48.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
-  tm_layout(frame = FALSE, main.title = "Census Tracts by Rural, Suburban, Urban Classification")
-tmap_save(tract_map_missing, "figs/tract_map_missing.png")
+  tm_layout(frame = FALSE, legend.show = FALSE)
 
-#### ZCTA Map ----
+tmap_save(tract_map_all_nolegend, "figs/tract_map_nolegend.png")
+
+# Legend only
+tract_map_all_onlylegend <-
+  tm_shape(usT.sf_all) +
+  tm_fill(col = "rurality", palette = rural.cols, colorNA = "#c7eae5", showNA = FALSE,
+          title = "Classification") +
+  tm_shape(states48.sf) +
+  tm_borders(alpha = 0.7, lwd = 0.5) +
+  tm_layout(frame = FALSE, legend.only = TRUE,
+            legend.title.size = 1.5,
+            legend.text.size = 1)
+
+tmap_save(tract_map_all_onlylegend, "figs/tract_map_legendonly.png")
+
+
+#### Tract Maps - Urban, Rural, Suburban -----
+
+# Color scheme
+# rural, suburban, urban
+rural.cols <- c("#c7eae5", "#5ab4ac", "#01665e")
+
+# Rural map
+rural <- usT.sf_all %>% filter(rurality == "Rural")
+
+rural_map <- 
+  tm_shape(rural) +
+  tm_fill(col = "rurality", palette = "#c7eae5",
+        title = "") +
+  tm_shape(usT.sf_all) + tm_borders(alpha = 0.3, lwd = 0.1) +
+  tm_shape(states48.sf) +
+  tm_borders(alpha = 1, lwd = 0.6) +
+  tm_layout(frame = FALSE,
+            legend.show = FALSE)
+
+tmap_save(rural_map, "figs/rural_map.png")
+
+# Suburban map
+suburban <- usT.sf_all %>% filter(rurality == "Suburban") 
+
+suburban_map <- 
+  tm_shape(suburban) +
+  tm_fill(col = "rurality", palette = "#5ab4ac",
+          title = "") +
+  tm_shape(usT.sf_all) + tm_borders(alpha = 0.3, lwd = 0.1) +
+  tm_shape(states48.sf) +
+  tm_borders(alpha = 1, lwd = 0.6) +
+  tm_layout(frame = FALSE,
+            legend.show = FALSE)
+
+tmap_save(suburban_map, "figs/suburban_map.png")
+
+# Urban map
+urban <- usT.sf_all %>% filter(rurality == "Urban")
+
+urban_map <- 
+tm_shape(urban) +
+  tm_fill(col = "rurality", palette = "#01665e",
+          title = "") +
+  tm_shape(usT.sf_all) + tm_borders(alpha = 0.3, lwd = 0.1) +
+  tm_shape(states48.sf) +
+  tm_borders(alpha = 1, lwd = 0.6) +
+  tm_layout(frame = FALSE,
+            legend.show = FALSE)
+
+tmap_save(urban_map, "figs/urban_map.png")
+
+
+#### ZCTA Maps ----
 
 # Load data
 rucaZ <- read.csv("data_final/UrbanSubRural_Z.csv")
@@ -195,27 +236,18 @@ rucaZ.sf <- st_transform(rucaZ.sf, crs)
 # Alaksa zip codes
 alaskaZ.sf <- rucaZ.sf %>% filter(STATE == "AK") %>% 
   st_transform(crs_alaska) 
-# %>%
-#   mutate(geometry = place_geometry(geometry, st_bbox(rucaZ.sf), 0.6, 1.35)) %>% 
-#   st_set_crs(crs)
 
 # Hawaii zip codes
 hawaiiZ.sf <- rucaZ.sf %>% filter(STATE == "HI") %>% 
   st_transform(crs_hawaii) 
-# %>%
-#   mutate(geometry = place_geometry(geometry, st_bbox(rucaZ.sf), 0.2, 0.1)) %>% 
-#   st_set_crs(crs)
 
 # Continental US zip codes
 usZ.sf <- rucaZ.sf %>% filter(!STATE %in% c("AK", "HI")) %>%
   st_transform(crs)
 
-# # Combine data
-# us_albers_alt <- rbind(usZ.sf, alaskaZ.sf, hawaiiZ.sf)
-
 # ZCTA Map
 
-# Create color scheme option
+# Color scheme
 # rural, suburban, urban
 rural.cols <- c("#c7eae5", "#5ab4ac", "#01665e")
 
@@ -226,6 +258,18 @@ ZCTA_map <-
   tm_shape(states48.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
   tm_layout(frame = FALSE, main.title = "ZCTAs by Rural, Suburban, Urban Classification")
+tmap_save(ZCTA_map, "figs/zcta_map.png")
+
+ZCTA_map_notitle <- 
+  tm_shape(usZ.sf) +
+  tm_fill(col = "rurality", palette = rural.cols,
+          title = "Classification") +
+  tm_shape(states48.sf) +
+  tm_borders(alpha = 0.7, lwd = 0.5) +
+  tm_layout(frame = FALSE,
+            legend.title.size = 1.5,
+            legend.text.size = 1)
+tmap_save(ZCTA_map_notitle, "figs/zcta_map_notitle.png")
 
 alaskaZ_map <-
   tm_shape(alaskaZ.sf) +
@@ -233,7 +277,6 @@ alaskaZ_map <-
   tm_shape(alaska.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
   tm_layout(frame = FALSE, legend.show = FALSE)
-alaskaZ_map
 
 hawaiiZ_map <-
   tm_shape(hawaiiZ.sf) +
@@ -241,12 +284,3 @@ hawaiiZ_map <-
   tm_shape(hawaii.sf) +
   tm_borders(alpha = 0.7, lwd = 0.5) +
   tm_layout(frame = FALSE, legend.show = FALSE)
-hawaiiZ_map
-
-tmap_save(ZCTA_map, "figs/ZCTA_map.png")
-tmap_save(alaskaZ_map, "figs/alaskaZCTA_map.png")
-tmap_save(hawaiiZ_map, "figs/hawaiiZCTA_map.png")
-
-rucaT.sf %>% filter(!rurality %in% c("Rural", "Suburban", "Urban"))
-
-which(is.na(usZ.sf$rurality))
